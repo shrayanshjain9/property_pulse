0b5395f13789fdf72c8491565a432b84d539e306
Shrayansh Jain <jainshrayansh1998@gmail.com>
2024-05-13 19:22:28 +0530

    Database connection & Display property details

---
+++ models/User.js
@@ -0,0 +1,31 @@
+import { Schema, models, model } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: [true, "Email Already Exists"],
+      required: [true, "Email is required"],
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+    },
+    image: {
+      type: String,
+    },
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Property",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = models.user || model("User", UserSchema);
+
+export default User;
